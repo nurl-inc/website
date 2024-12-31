823b08e10326151ba08021bb95dbae29a6b8160f
caseybaggz <caseybaggz@gmail.com>
2024-12-31 10:15:36 -0600

    feat: add error boundary

---
+++ src/components/shared/page-error.tsx
@@ -0,0 +1,44 @@
+import { Box, Container, VStack } from 'styled-system/jsx';
+import { Button, Text } from '~/components/ui';
+import { Footer, Head, Nav, Main } from '.';
+
+interface PageErrorProps {
+  error: {
+    message?: string;
+  };
+  reset: () => void;
+}
+
+export default function PageError(props: PageErrorProps) {
+  return (
+    <>
+      <Head
+        title="Nurl | Server Error"
+        description="The server is experiencing issues."
+      />
+      <Nav />
+
+      <Main>
+        <Container>
+          <VStack h="calc(100dvh - 100px)" justify="center">
+            <Text as="h1" textStyle="heading-xl">
+              Crit Fail
+            </Text>
+            <Text textStyle="2xl">
+              The server is experiencing issues. Please try again later.
+            </Text>
+            <Text textStyle="body-lg">
+              {props.error.message || 'An unknown error occurred.'}
+            </Text>
+
+            <Box marginBlockStart="6" w="full">
+              <Button onClick={() => props.reset()}>Try Again</Button>
+            </Box>
+          </VStack>
+        </Container>
+      </Main>
+
+      <Footer />
+    </>
+  );
+}
