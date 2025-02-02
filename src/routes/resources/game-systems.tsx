318c7836f13d87aa05144656e6c87df47376b30f
caseybaggz <caseybaggz@gmail.com>
2025-02-02 09:30:02 -0600

    chore: add missing page templates and 404 status code

--- src/routes/resources/game-systems.tsx
+++ src/routes/resources/game-systems.tsx
@@ -1,5 +1,4 @@
 import type { RouteSectionProps } from '@solidjs/router';
-import { HttpStatusCode } from '@solidjs/start';
 import { Container, VStack } from 'styled-system/jsx';
 import Footer from '~/components/shared/footer';
 import Head from '~/components/shared/head';
@@ -7,11 +6,15 @@ import Main from '~/components/shared/main';
 import Nav from '~/components/shared/nav';
 import { Text } from '~/components/ui';
 import type { Metadata } from '~/types';
+import keywords from '~/data/keywords.json';
+
+// Route Data
 
 const metadata: Metadata = {
-  title: 'Nurl | Page Not Found',
-  description: 'The page you are looking for does not exist.',
-  keywords: 'nurl, page not found, 404',
+  title: 'Nurl Game Systems | Game System Library',
+  description:
+    'Nurl is an end-to-end solution for building, testing, and validating your tabletop roleplay games in a safe and version-controlled environment.',
+  keywords: keywords.base.join(', '),
   image: 'https://nurlttrpg.com/og-meta.png',
 };
 
@@ -23,14 +26,15 @@ export const route = {
   },
 };
 
+// Component
+
 interface RouteData {
   metadata: Metadata;
 }
 
-export default function NotFound(props: RouteSectionProps<RouteData>) {
+export default function GameSystems(props: RouteSectionProps<RouteData>) {
   return (
     <>
-      <HttpStatusCode code={404} />
       <Head {...props.data.metadata} />
       <Nav />
 
@@ -38,12 +42,12 @@ export default function NotFound(props: RouteSectionProps<RouteData>) {
         <Container>
           <VStack h="calc(100dvh - 100px)" justify="center">
             <Text as="h1" textStyle="heading-xl">
-              Coming Soon
+              Nurl Game Systems
             </Text>
-            <Text textStyle="2xl">You've stumbled into the void.</Text>
+            <Text textStyle="2xl">We're working on it.</Text>
             <Text textStyle="body-lg">
-              The page you are looking for does not exist but we're working on
-              it.
+              Once we have launched, you'll be able to find a library of game
+              systems we support here.
             </Text>
           </VStack>
         </Container>
